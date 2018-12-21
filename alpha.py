894dcdd9ee6c4960f5e7df7e10810a00ff6968c8
sherlinbency <45781622+sherlinbency@users.noreply.github.com>
2018-12-21 15:31:18 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char c;
+   printf("enter a character:");
+   scanf("%c",&c);
+   printf("%c is an alphabet",c);
+   else
+       printf("%c is not an alphabet",c);
+       return 0;
+ }
