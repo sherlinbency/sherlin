3911b035d120d2bca12556e3aa0760be1a0de819
sherlinbency <45781622+sherlinbency@users.noreply.github.com>
2019-02-11 13:26:22 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,9 @@
+if number > 1:
+ if (number % i) == 0:
+    print(number, "is not a prime number")
+ else:
+   print(number, "is a prime number")
+ if (number % i) == 0:
+    print(number, "is not a prime number")
+  
+       
